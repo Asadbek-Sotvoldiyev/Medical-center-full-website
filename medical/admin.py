e24714e8aa90a225c6f13f9e5abda30166d14774
Asadbek Sotvoldiyev <blogasadbek@gmail.com>
2024-05-15 21:36:19 +0500

    admin panel changed

--- medical/admin.py
+++ medical/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Family, Person, Register
 
 
-admin.site.register([Family, Person, Register])
+class FamilyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'profession', 'phone_number', 'gender')
+
+
+admin.site.register(Family, FamilyAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Register)
